307e2580b6e41cbd9bfc0bf7aeec7930f84be58f
Abhishek313 <vabhi5149@gmail.com>
2024-03-30 23:05:06 +0530

    complete authorization

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,19 @@
 import express ,{Request,Response} from 'express';
+import mongoose from 'mongoose';
 import cors from 'cors';
 import 'dotenv/config';
+import myUserRoute from './routes/MyUserRoute'
+
+
+mongoose.connect(process.env.MONGODB_URI as string)
+.then(() => console.log("connected to database!"))
  
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/test", async(req: Request ,res: Response) => {
-   res.json({
-    message: "Hello"
-   })
-})
+app.use("/api/my/user",myUserRoute)
 
 app.listen(8000,() => {
     console.log(`Server is running on port 8000`);
